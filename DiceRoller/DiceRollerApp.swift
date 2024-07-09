4f82d0f32e403cfbe6d4cd161fb95a25c84588dc
Гамид Халидов <khalidov-gamid@mail.ru>
2024-07-09 12:57:33 +0300

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Gamıd Khalıdov on 09.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
